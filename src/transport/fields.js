9129fcd05ea3c2a2f973693d4e47e190311b884f
faizanvahevaria <faizanvahevaria@gmail.com>
2019-10-10 18:13:13 +0530

    Separated fields, datatype file

---
+++ src/transport/fields.js
@@ -0,0 +1,21 @@
+const fields={
+    'type':'type',
+    'name':'name',
+    'description':'description',
+    'version':'version',
+    'status':'status',
+    'Required':'Required',
+    'Property':'Property',
+    'isID':'isID',
+    'status':'status',
+    'pattern':'pattern',
+    'DataType':'DataType',
+    'cardinality':'cardinality',
+    'enum':'enum',
+    'Relationship':'Relationship',
+    'source':'source',
+    'target':'target',
+    'association':'association',
+    'class':'class'
+};
+module.exports=fields;
--- src/transport/transport.js
+++ src/transport/transport.js
@@ -1,5 +1,7 @@
 var forEach = require('async-foreach').forEach;
 var constant = require('./constant');
+var fields = require('./fields');
+var datatype = require('./datatype');
 var path = require('path');
 const fs = require('fs');
 const CircularJSON = require('circular-json');
@@ -304,25 +306,27 @@ function exportModel() {
                         console.log("Filename : ", filename);
                         let packageString=app.repository.writeObject(umlPackage);
                         let allEntities=app.repository.select(umlPackage.name+'::@UMLClass')
-                        let jsonProcess={
+                        /* ddd let jsonProcess={
                             'type':'Package',
                             'name':umlPackage.name
-                        };
+                        }; */
+                        jsonProcess[fields.type]='Package';
+                        jsonProcess[fields.name]=umlPackage.name;
 
                         forEach(allEntities,function(entity){
 
                             /* Entity binding--- */
                             let entityObj={};
                             jsonProcess[entity.name]=entityObj;
-                            entityObj['type']=utils.getElementType(entity);
-                            entityObj['name']=entity.name;
-                            entityObj['description']=entity.documentation;
-                            entityObj['version']='';
-                            entityObj['status']='';
+                            entityObj[fields.type]=utils.getElementType(entity);
+                            entityObj[fields.name]=entity.name;
+                            entityObj[fields.description]=entity.documentation;
+                            entityObj[fields.version]='';
+                            entityObj[fields.status]='';
 
                             /* Required property binding */
                             let requiredArr=[];
-                            entityObj['required']=requiredArr;
+                            entityObj[fields.Required]=requiredArr;
                             let attributeForRequired=entity.attributes;
                             forEach(attributeForRequired,function(attrForRequired){
                                 if (attrForRequired.multiplicity == "1" || attrForRequired.multiplicity == "1..*") {
@@ -332,51 +336,51 @@ function exportModel() {
 
                             /* Property binding--- */
                             let propertyArr=[];
-                            entityObj['Property']=propertyArr;
+                            entityObj[fields.Property]=propertyArr;
                             let attribute=entity.attributes;
                             forEach(attribute,function(attr){
                                 let propertyObj={};
-                                propertyObj['name']=attr.name;  
+                                propertyObj[fields.name]=attr.name;  
 
-                                propertyObj['description']=attr.documentation;
+                                propertyObj[fields.description]=attr.documentation;
 
                                 if(attr.isID){
-                                    propertyObj['isID']=attr.isID;
+                                    propertyObj[fields.isID]=attr.isID;
                                 }
 
-                                propertyObj['status']='';
-                                
+                                propertyObj[fields.status]='';
+                                --1
                                 /* DataType binding--- */
                                 let dType={};
                                 if(utils.isString(attr.type)){
-                                    if(attr.type=='URL'){
-                                        dType["pattern"]=constant.regex_email;
+                                    if(attr.type==datatype.url){
+                                        dType[fields.pattern]=constant.regex_email;
                                     }
 
                                     dType.type=attr.type;
-                                    propertyObj['DataType']=dType;
-                                    dType['name']=attr.name;
-                                    dType['cardinality']=attr.multiplicity;
+                                    propertyObj[fields.DataType]=dType;
+                                    dType[fields.name]=attr.name;
+                                    dType[fields.cardinality]=attr.multiplicity;
                                 }else if(attr.type instanceof type.UMLClass){
-                                    propertyObj['DataType']=dType;
+                                    propertyObjfields.DataType=dType;
                                     dType.type=utils.getElementType(attr.type);
-                                    dType['name']=attr.type.name;
-                                    dType['cardinality']=attr.multiplicity;
+                                    dType[fields.name]=attr.type.name;
+                                    dTypefields.cardinality=attr.multiplicity;
                                 }else if(attr.type instanceof type.UMLEnumeration){
-                                    propertyObj['DataType']=dType;
+                                    propertyObjfields.DataType=dType;
                                     dType.type=utils.getElementType(attr.type);
-                                    dType['name']=attr.type.name;
-                                    dType['cardinality']=attr.multiplicity;
+                                    dType[fields.name]=attr.type.name;
+                                    dTypefields.cardinality=attr.multiplicity;
 
                                     /* binding literals  */
                                     let arrliterals=[];
-                                    dType['enum']=arrliterals;
+                                    dType[fields.enum]=arrliterals;
                                     let literals=attr.type.literals;
                                     forEach(literals,function(itemLiterals){
                                         arrliterals.push(itemLiterals.name);
                                     });
 
-                                    dType['cardinality']=attr.multiplicity;
+                                    dTypefields.cardinality=attr.multiplicity;
                                 }
                                 
                                 propertyArr.push(propertyObj);
@@ -384,12 +388,12 @@ function exportModel() {
 
                             /* Relationship binding */
                             let Relationship=[];
-                            entityObj['Relationship']=Relationship;
+                            entityObj[fields.Relationship]=Relationship;
                             forEach(entity.ownedElements,function(element){
                                 let objRelationship={};
                                 
-                                objRelationship['name']=element.name;
-                                objRelationship['description']=element.documentation;
+                                objRelationship[fields.name]=element.name;
+                                objRelationship[fields.description]=element.documentation;
 
                                 if(element instanceof type.UMLAssociation){
 
@@ -397,77 +401,77 @@ function exportModel() {
                                     let end2=element.end2;
                                     if(end1.aggregation=='shared' && end2.aggregation=='none'){
                                         /* aggregation */
-                                        objRelationship['type']='aggregation';
+                                        objRelationship[fields.type]='aggregation';
                                     } else if(end1.aggregation=='composite' && end2.aggregation=='none'){
                                         /* composition */
-                                        objRelationship['type']='composition';
+                                        objRelationship[fields.type]='composition';
                                     }
 
                                     let objSource={};
                                     let source=end1.reference;
-                                    objRelationship['source']=objSource;
-                                    objSource['name']=source.name;
-                                    objSource['type']=utils.getElementType(source);
+                                    objRelationship[fields.source]=objSource;
+                                    objSource[fields.name]=source.name;
+                                    objSource[fields.type]=utils.getElementType(source);
 
                                     let objTarget={};
                                     let target=end2.reference;
-                                    objRelationship['target']=objTarget;
-                                    objTarget['name']=target.name;
-                                    objTarget['type']=utils.getElementType(target);
+                                    objRelationship[fields.target]=objTarget;
+                                    objTarget[fields.name]=target.name;
+                                    objTarget[fields.type]=utils.getElementType(target);
 
                                 } else if(element instanceof type.UMLGeneralization){
 
-                                    objRelationship['type']=utils.getElementType(element);
+                                    objRelationship[fields.type]=utils.getElementType(element);
 
                                     let objSource={};
                                     let source=element.source;
-                                    objRelationship['source']=objSource;
-                                    objSource['name']=source.name;
-                                    objSource['type']=utils.getElementType(source);
+                                    objRelationship[fields.source]=objSource;
+                                    objSource[fields.name]=source.name;
+                                    objSource[fields.type]=utils.getElementType(source);
 
 
                                     let objTarget={};
                                     let target=element.target;
-                                    objRelationship['target']=objTarget;
-                                    objTarget['name']=target.name;
-                                    objTarget['type']=utils.getElementType(target);
+                                    objRelationship[fields.target]=objTarget;
+                                    objTarget[fields.name]=target.name;
+                                    objTarget[fields.type]=utils.getElementType(target);
 
                                 } else if(element instanceof type.UMLAssociationClassLink){
 
-                                    objRelationship['type']=utils.getElementType(element);
+                                    objRelationship[fields.type]=utils.getElementType(element);
                                     let objAssociation={};
                                     /* association binding */
-                                    objRelationship['association']=objAssociation;
+                                    objRelationship[fields.association]=objAssociation;
 
                                     let end1=element.associationSide.end1;
                                     let end2=element.associationSide.end2;
                                     if(end1.aggregation=='shared' && end2.aggregation=='none'){
                                         /* aggregation */
-                                        objAssociation['type']='aggregation';
+                                        objAssociation[fields.type]='aggregation';
                                     } else if(end1.aggregation=='composite' && end2.aggregation=='none'){
                                         /* composition */
-                                        objAssociation['type']='composition';
+                                        objAssociation[fields.type]='composition';
                                     }
 
                                     let objSource={};
                                     let source=end1.reference;
-                                    objAssociation['source']=objSource;
-                                    objSource['name']=source.name;
-                                    objSource['type']=utils.getElementType(source);
+                                    objAssociation[fields.source]=objSource;
+                                    objSource[fields.name]=source.name;
+                                    objSource[fields.type]=utils.getElementType(source);
 
                                     let objTarget={};
                                     let target=end2.reference;
-                                    objAssociation['target']=objTarget;
-                                    objTarget['name']=target.name;
-                                    objTarget['type']=utils.getElementType(target);
+                                    objAssociation[fields.target]=objTarget;
+                                    objTarget[fields.name]=target.name;
+                                    objTarget[fields.type]=utils.getElementType(target);
 
                                     /* class side association binding */
                                     let objClass={};
                                     let classSide=element.classSide;
-                                    objRelationship['class']=objClass;
+                                    objRelationship[fields.class]=objClass;
 
-                                    objClass['name']=classSide.name
-                                    objClass['type']=utils.getElementType(classSide);
+                                    objClass[fields.name]=classSide.name
+                                    objClass[fields.type]=utils.getElementType(classSide);
 
                                     /* association class side properties */
                                     /* Property binding--- */
@@ -477,9 +481,9 @@ function exportModel() {
                                     let attribute=classSide.attributes;
                                     forEach(attribute,function(attr){
                                         let propertyObj={};
-                                        propertyObj['name']=attr.name;  
+                                        propertyObj[fields.name]=attr.name;  
 
-                                        propertyObj['description']=attr.documentation;
+                                        propertyObj[fields.description]=attr.documentation;
 
                                         propertyObj['status']='';
                                         
@@ -488,29 +492,29 @@ function exportModel() {
                                         if(utils.isString(attr.type)){
 
                                             dType.type=attr.type;
-                                            propertyObj['DataType']=dType;
-                                            dType['name']=attr.name;
-                                            dType['cardinality']=attr.multiplicity;
+                                            propertyObjfields.DataType=dType;
+                                            dType[fields.name]=attr.name;
+                                            dTypefields.cardinality=attr.multiplicity;
                                         }else if(attr.type instanceof type.UMLClass){
-                                            propertyObj['DataType']=dType;
+                                            propertyObjfields.DataType=dType;
                                             dType.type=utils.getElementType(attr.type);
-                                            dType['name']=attr.type.name;
-                                            dType['cardinality']=attr.multiplicity;
+                                            dType[fields.name]=attr.type.name;
+                                            dTypefields.cardinality=attr.multiplicity;
                                         }else if(attr.type instanceof type.UMLEnumeration){
-                                            propertyObj['DataType']=dType;
+                                            propertyObjfields.DataType=dType;
                                             dType.type=utils.getElementType(attr.type);
-                                            dType['name']=attr.type.name;
-                                            dType['cardinality']=attr.multiplicity;
+                                            dType[fields.name]=attr.type.name;
+                                            dTypefields.cardinality=attr.multiplicity;
 
                                             // binding literals  
                                             let arrliterals=[];
-                                            dType['enum']=arrliterals;
+                                            dType[fields.enum]=arrliterals;
                                             let literals=attr.type.literals;
                                             forEach(literals,function(itemLiterals){
                                                 arrliterals.push(itemLiterals.name);
                                             });
 
-                                            dType['cardinality']=attr.multiplicity;
+                                            dTypefields.cardinality=attr.multiplicity;
                                         }
                                         propertyArr.push(propertyObj);
                                     });
@@ -521,20 +525,20 @@ function exportModel() {
 
 
                                 }else if(element instanceof type.UMLInterfaceRealization){
-                                    objRelationship['type']=utils.getElementType(element);
+                                    objRelationship[fields.type]=utils.getElementType(element);
 
                                     let objSource={};
                                     let source=element.source;
-                                    objRelationship['source']=objSource;
-                                    objSource['name']=source.name;
-                                    objSource['type']=utils.getElementType(source);
+                                    objRelationship[fields.source]=objSource;
+                                    objSource[fields.name]=source.name;
+                                    objSource[fields.type]=utils.getElementType(source);
 
 
                                     let objTarget={};
                                     let target=element.target;
-                                    objRelationship['target']=objTarget;
-                                    objTarget['name']=target.name;
-                                    objTarget['type']=utils.getElementType(target);
+                                    objRelationship[fields.target]=objTarget;
+                                    objTarget[fields.name]=target.name;
+                                    objTarget[fields.type]=utils.getElementType(target);
                                 }
                                 Relationship.push(objRelationship);
                             })
